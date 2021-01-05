6a9d77804036fbb3b58029b8c7f19a6ae13ac168
ElderVivot <eldervivot@gmail.com>
2021-01-04 22:05:04 -0300

    feat: update to use npm instead yarn. Add to create of folder deleting chars special

--- src/utils/CreateFolderToSaveData.ts
+++ src/utils/CreateFolderToSaveData.ts
@@ -4,6 +4,7 @@ import 'dotenv/config'
 
 // import GetSettingsWayFiles from '../controllers/GetSettingsWayFiles'
 import ISettingsGoiania from '../models/ISettingsGoiania'
+import { treateTextField } from './functions'
 
 const mountFolder = (settings: ISettingsGoiania, folder: string) => {
     let newFolder = folder
@@ -13,7 +14,7 @@ const mountFolder = (settings: ISettingsGoiania, folder: string) => {
         newFolder = folder.replace(/[\\]/g, '/')
     }
 
-    const nameCompanie = settings.companie ? settings.companie.trim().normalize('NFD').replace(/([\u0300-\u036f]|[^0-9a-zA-Z ])/g, '').toUpperCase().substring(0, 70) : undefined
+    const nameCompanie = settings.companie ? treateTextField(settings.companie).substring(0, 70) : undefined
     // const nameCompanie = settings.companie ? settings.companie.substring(0, 70) : undefined
 
     const folderSplit = newFolder.split('/')
